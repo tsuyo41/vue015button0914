2ab6b502c389187671b6b9f9ca12a7e0b53db8b7
tsuyoshi fukunaga <sankakutaiyo@yahoo.co.jp>
2020-09-15 23:37:02 +0900

    Buttonコンポーネント作成

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <div id="app">
     <h1>hello</h1>
+    <Button />
+    <Button />
+    <Button />
+    
   </div>
 </template>
 
 <script>
+import Button from './components/Button'
+
+export default {
+  name: 'fukufuku',
+  components: {
+    Button
+  }
+}
 
 </script>
 
---
+++ src/components/Button.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="button">
+      ボタン
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .button {
+        display: inline-block;
+        background: red;
+        border-radius: 5px;
+        color: white;
+        padding: 10px;
+        cursor: pointer;
+        margin-right: 5px;
+    }
+
+    .button:hover {
+        background: rgb(182, 0, 0);
+    }
+
+
+    .button:active {
+        background: rgb(138, 13, 13);
+    }
+</style>
