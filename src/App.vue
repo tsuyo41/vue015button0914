4a7cba09aa3509f732705261388c9191f3588493
tsuyoshi fukunaga <sankakutaiyo@yahoo.co.jp>
2020-09-17 21:37:45 +0900

    Counter.vueを作成

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
   <div id="app">
     <h1>hello</h1>
-    <Button />
-    <Button />
-    <Button />
+    <Counter 
+      buttonTitle="ボタン1"
+    />
+    <Counter 
+      button-title="ボタン2"
+      :initial-count="10"
+    />
+    <Counter 
+      buttonTitle="ボタン3"
+      :initialCount="100"
+    />
     
   </div>
 </template>
 
 <script>
-import Button from './components/Button'
+// import Button from './components/Button'
+import Counter from './components/Counter'
 
 export default {
   name: 'fukufuku',
   components: {
-    Button
+    // Button,
+    Counter
   }
 }
 
---
+++ src/components/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <p>カウント： {{ count }}</p>
+      <button @click="count++">{{ buttonTitle }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    props: {
+        buttonTitle: {
+            type: String,
+            required: true,
+
+        },
+        initialCount: {
+            type: Number,
+            required: false,
+            default: 0
+        }
+    },
+    data() {
+        return {
+            count: this.initialCount
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
